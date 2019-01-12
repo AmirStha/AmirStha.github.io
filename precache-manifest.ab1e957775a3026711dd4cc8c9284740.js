676054dbee1b16ce096c8863077c94b74f5e8ead
AmirStha (from Travis CI) <amirshresthapj@gmail.com>
2019-01-12 18:23:12 +0000

    Deploy AmirStha/AmirStha.github.io to github.com/AmirStha/AmirStha.github.io.git:master

---
+++ precache-manifest.ab1e957775a3026711dd4cc8c9284740.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6fc219fd9a3b8bc4daf0",
+    "url": "/static/css/main.8415fe12.chunk.css"
+  },
+  {
+    "revision": "6fc219fd9a3b8bc4daf0",
+    "url": "/static/js/main.6fc219fd.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "75ace7b577cadd407ca61a6c0d48fb13",
+    "url": "/index.html"
+  }
+];
